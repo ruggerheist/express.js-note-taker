5efda07a4d1bdc27701df1d5d1432f548692da58
Rick Heist <rick.heist@gmail.com>
2023-05-28 23:45:31 -0400

    cleaned up server.js, deleted white space

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ const path = require('path');
 const fs = require('fs');
 const crypto = require('crypto');
 const notesDb = require('./db/db.json');
-
-
-
 const app = express();
 const PORT = 3001;
 
-// TODO: Invoke app.use() and serve static files from the '/public' folder
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
@@ -20,12 +16,9 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-// TODO: Create a route that will serve up the `public/paths.html` page
 app.get('/api/notes', (req, res) => {
   res.json(notesDb);
 });
-
-
 
 app.post('/api/notes', (req, res) => {
   const { title, text } = req.body
@@ -41,15 +34,6 @@ if (title && text) {
   res.json(newNote);
   console.log(notesDb);
   const noteString = JSON.stringify(notesDb);
-  //  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.error(err);
-  //   } else {
-  //       console.log(data);
-  //     const parsedData = JSON.parse(data);
-  //     console.log(req.body);
-  //     parsedData.push(req.body());
-  //     console.log(parsedData);
       fs.writeFile('./db/db.json', noteString, (err) => 
       err ? console.error(err) : console.log(`New Note Has Been Saved`)              
     )
@@ -57,7 +41,7 @@ if (title && text) {
   };    
 });
 
-app.delete(`/api/notes/:id`, (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
  const { id } = req.params;
  const noteIndex = notesDb.findIndex(notes => notes.id == id);
  notesDb.splice(noteIndex, 1);
